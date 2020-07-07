98d117d0cbb0f7e9cb99cb70ff523b74b943801b
unknown <quentin-poirier@live.fr>
2020-07-07 15:53:37 +0200

    créa accueil form connexion et inscription

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <title>Titre Pro</title>
+    <style>
+        .header {
+            height: 170px;
+            width: auto;
+        }
+    </style>
+</head>
+
+<body>
+    <header class="d-flex flex-row align-items-center justify-content-center bg-white header">
+        <div class="d-flex">
+            <div class="text-dark h2">TITRE & LOGO</div>
+        </div>
+    </header>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark mb-3">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Association</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Bénévoles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <form class="form-inline">
+            <div class="form-group mx-sm-3">
+                <button class="btn btn-outline-secondary text-white ml-2" type="button" data-toggle="modal" data-target="#connexionModal">connexion</button>
+            </div>
+            <div class="form-group">
+                <button class="btn btn-outline-secondary text-white" type="button" data-toggle="modal" data-target="#inscriptionModal">inscription</button>
+            </div>
+        </form>
+    </nav>
+
+    <main>
+        <div class="container-fluid">
+            <div class="row justify-content-center mb-3">
+                <div class="col text-uppercase h2 text-dark text-center">à la une</div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card w-100 shadow rounded">
+                        <img src="assets\img\aquacaux.jpg" class="card-img-top" style="height: 18rem;" alt="aquacaux">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">site web</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card w-100 shadow rounded">
+                        <img src="assets\img\graineEnMain.jpg" class="card-img-top" style="height: 18rem;" alt="graineenmain">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">site web</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="card w-100 shadow rounded">
+                        <img src="assets\img\médiaction.jpg" class="card-img-top" style="height: 18rem;" alt="médiaction">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-dark">site web</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+
+    <!-- modal connexion -->
+    <div class="modal fade" id="connexionModal" tabindex="-1" role="dialog" aria-labelledby="inscriptionModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div id="articleModalHeader" class="modal-header bg-dark">
+                    <div class="modal-title text-white h4" id="exampleModalLabel">Formulaire d'inscription</div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form class="mb-3 bg-light p-5" action="index.php" method="post" novalidate>
+                    <div class="form-group">
+                        <label for="login" class="text-secondary font-weight-bold">Identifiant</label>
+                        <input type="text" class="form-control" id="login" name="login" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="text-secondary font-weight-bold">Mot de passe</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" name="submit" class="btn btn-outline-secondary">Se connecter</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- modal connexion -->
+
+    <!-- modal d'inscription -->
+    <div class="modal fade" id="inscriptionModal" tabindex="-1" role="dialog" aria-labelledby="inscriptionModal" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div id="articleModalHeader" class="modal-header bg-dark">
+                    <div class="modal-title text-white h4" id="exampleModalLabel">Formulaire d'inscription</div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form class="mb-3 bg-light p-5" action="index.php" method="post" novalidate>
+                    <div class="form-group">
+                        <label for="login" class="text-secondary font-weight-bold">Adresse mail</label>
+                        <input type="email" class="form-control" id="login" name="login" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="login" class="text-secondary font-weight-bold">Identifiant</label>
+                        <input type="text" class="form-control" id="login" name="login" required>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="text-secondary font-weight-bold">Mot de passe</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <div class="text-center">
+                        <button type="submit" name="submit" class="btn btn-outline-secondary">S'inscrire</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- modal d'inscription -->
+
+    <footer class="d-flex flex-row align-items-center justify-content-center bg-dark header mt-3">
+        <div class="d-flex">
+            <div class="text-white h4">mentions légales / contact / acceuil</div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+
+</html>
